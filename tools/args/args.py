c3e2ec0079e656ca0cce8099728ab72d67b31e01
bluceoy <bluceoy@126.com>
2020-10-22 10:42:36 +0800

    update

--- tools/args/args.py
+++ tools/args/args.py
@@ -1,3 +1,12 @@
 import time
+import sys
+
+import logging
+
+log = logging.getLogger(__name__)
 
 print("now = %f" % (time.time()))
+print("args = %s" % (sys.argv))
+
+log.info("now = %f", time.time())
+log.info("args = %s", sys.argv)
