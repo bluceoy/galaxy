b647aa9b733fae00422ca2ea46a3ea5ff3452199
bluceoy <bluceoy@126.com>
2020-10-22 10:11:05 +0800

    update

---
+++ tools/custom/job_agent.py
@@ -0,0 +1,36 @@
+import logging
+import psycopg2
+import subprocess
+import os
+import sys
+import shutil
+import subprocess
+
+log = logging.getLogger(__name__)
+
+
+class JobAgent(object):
+  def __init__(self, job_id, tool_id, cwd):
+    self.host = '192.168.0.184'
+    self.port = 5432
+    self.user = 'postgres'
+    self.password = '123456'
+    self.db = 'postgres'
+    self.job_id = job_id
+    self.tool_id = tool_id
+    self.cwd = cwd
+    
+  def run(self, *args):
+    commands = []
+    commands.append("python")
+    commands.append(*args)
+    process = subprocess.run(commands, cwd=self.cwd, check=True)
+    log.info("returncode = %d", process.returncode)
+
+if __name__ == "__main__":
+  job_id = sys.argv[1]
+  tool_id = sys.argv[2]
+  cwd = sys.argv[3]
+  args = sys.argv[4:]
+  job = JobAgent(job_id, tool_id, cwd)
+  job.run(*args)
