775b4d7f513caec7df19ea88687ec2a3aa81c89f
xianmau <xianmaulin@gmail.com>
2020-10-26 18:53:58 +0800

    update

--- client/galaxy/scripts/components/ARGs/Results.vue
+++ client/galaxy/scripts/components/ARGs/Results.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="file-wrap">
+    <div class="result-wrap">
         <b-table :fields="fields" :items="items" :bordered="true">
             <template v-slot:cell(name)="data">
                 <a href="#">{{ data.value }}</a>
@@ -14,29 +14,40 @@
                 {{ statusMap[data.value] }}
             </template>
             <template v-slot:cell(action)="data">
-                <a @click="onView(data.item.job_id)">View</a>
+                <a href="#" @click="onView(data.item.job_id)">View</a>
+                <!-- <a @click="onView(data.item.job_id)">View</a>
                 |
                 <a>Rerun</a>
                 |
-                <a @click="onDownload(data.item.output)">Download</a>
+                <a @click="onDownload(data.item.output)">Download</a> -->
             </template>
         </b-table>
 
-        <b-modal v-model="modalShow" static no-enforce-focus hide-footer>
+        <b-modal v-model="modalShow" static no-enforce-focus hide-footer size="lg">
             <template v-slot:modal-header>
                 <h4 class="title" tabindex="0">Job Detail</h4>
             </template>
-            <div class="my-tool-wrap">
-                <div>Job Id: {{ job.job_id }}</div>
-                <div>Job Status: {{ statusMap[job.status] }}</div>
-                <div>Tool Id: {{ job.tool_id }}</div>
-                <div>Tool Version: {{ job.tool_version }}</div>
-                <div>Tool Name: {{ job.tool_name }}</div>
-                <div>Input Params: {{ job.parmas }}</div>
-                <div>Output File: {{ job.output }}</div>
+            <div class="result-detail">
+                <div class="line">
+                    <div><span>Job Id:</span> {{ job.job_id }}</div>
+                    <div><span>Job Status:</span> {{ statusMap[job.status] }}</div>
+                </div>
+                <div class="line">
+                    <div><span>Tool Id:</span> {{ job.tool_id }}</div>
+                    <div><span>Tool Version:</span> {{ job.tool_version }}</div>
+                </div>
+                <div class="line">
+                    <div><span>Tool Name:</span> {{ job.tool_name }}</div>
+                    <div><span>Input Params:</span> {{ job.params }}</div>
+                </div>
+                <div class="line">
+                    <div><span>Output File:</span> {{ job.output }}</div>
+                </div>
+            </div>
+            <div class="result-visualize">
                 <h4>Visualize: </h4>
                 <div>
-                    // chart
+                    
                 </div>
             </div>
         </b-modal>
@@ -74,6 +85,11 @@ export default {
             ],
             items: [],
             job: {},
+            vData: {
+                type: 1, // 1:txt, 2:pdf, 3:jpg, 4:other
+                url: '',
+                data: {}
+            },
             pageNo: 1,
             pageSize: 100,
             total: 0
@@ -99,6 +115,8 @@ export default {
                 .get(`${Galaxy.root}api/custom/job/detail/${id}`)
                 .then((response) => {
                     this.job = response.data
+                    // 获取结果，如果支持可视化，则进行渲染
+                    this.onDownload(this.job.output)
                     this.modalShow = true
                 })
                 .catch((error) => {
@@ -111,7 +129,7 @@ export default {
             axios
                 .post(`${Galaxy.root}api/file/download`, { path: path })
                 .then((response) => {
-                    console.log(response)
+                    this.vData.data = response.data
                 })
                 .catch((error) => {
                     const message = error.response.data && error.response.data.err_msg;
@@ -130,7 +148,23 @@ export default {
 </script>
 
 <style scoped>
-.file-wrap {
+.result-wrap {
     margin-bottom: 40px;
+}
+.result-wrap .result-detail .line {
+    display: flex;
+    justify-content: space-between;
+}
+.result-wrap .result-detail .line > div {
+    width: 50%;
+}
+.result-wrap .result-detail .line div > span{
+    font-weight: bold;
+    display: inline-block;
+    width: 100px;
+}
+.result-wrap .result-visualize {
+    margin-top: 30px;
+    width: 100%;
 }
 </style>
