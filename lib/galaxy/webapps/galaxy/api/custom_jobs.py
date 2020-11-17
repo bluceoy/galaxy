ec6dfef9c7afe586e61d954909470f9d041ec934
bluceoy <bluceoy@126.com>
2020-11-17 20:08:35 +0800

    update

--- lib/galaxy/webapps/galaxy/api/custom_jobs.py
+++ lib/galaxy/webapps/galaxy/api/custom_jobs.py
@@ -282,78 +282,68 @@ class CustomJobsAPIController(BaseAPIController):
     return {"message": "ok", "job_id": job_id}
 
   def __on_mst(self, trans, payload, **kwargs):
-    missing_arguments = []
+    tool_name = "args_oap_v1.0"
+    tool_dir = "MST"
+    script = "mst.py"
     tool_id = payload.get("tool_id", None)
-    if not tool_id:
-      missing_arguments.append("tool_id")
-
     tool_version = payload.get("tool_version", None)
-    if not tool_version:
-      missing_arguments.append("tool_version")
-    
     input1 = payload.get("input1", None)
-    if not input1:
-      missing_arguments.append("input1")
-
     input2 = payload.get("input2", None)
-    if not input2:
-      missing_arguments.append("input2")
-
     input3 = payload.get("input3", None)
-    if not input3:
-      missing_arguments.append("input3")
-
     input4 = payload.get("input4", None)
-    if not input4:
-      missing_arguments.append("input4")
 
-    if len(missing_arguments) > 0:
-      raise ActionInputError("The following required arguments are missing in the payload: {}".format(missing_arguments))
 
-    input1_dir = os.path.dirname(input1)
-    log.info("input1 = %s, input1_dir = %s", input1, input1_dir)
+    self.check_args(tool_id, tool_version, 
+      input1, input2, input3, input4)
+
     root_dir = self.make_sure_root(trans)
+
     real_path1 = root_dir + input1
-    log.info("real_path1 = %s", real_path1)
     if not os.path.isfile(real_path1):
       raise ActionInputError("input1 not exist")
 
-    input1dir = os.path.dirname(real_path1)
-    output1 = input1_dir + "/output/output1.txt"
-    output2 = input1_dir + "/output/output2.txt"
-    output1_path = input1dir + "/output/output1.txt"
-    output2_path = input1dir + "/output/output2.txt"
+
+    input1_dir = os.path.dirname(input1)
+    output_dir = self.make_sure_output_dir(tool_name, root_dir, input1_dir)
+    output1 = self.make_output_file(output_dir, 1)
+    output2 = self.make_output_file(output_dir, 2)
+    output3 = self.make_output_file(output_dir, 3)
+    output4 = self.make_output_file(output_dir, 4)
+
+    abspath_output1 = root_dir + output1
+    abspath_output2 = root_dir + output2
+    abspath_output3 = root_dir + output3
+    abspath_output4 = root_dir + output4
 
     agent_cwd = trans.app.config.tool_path + "/custom"
-    job_cwd = trans.app.config.tool_path + "/MST"
+    job_cwd = trans.app.config.tool_path + "/" + tool_dir
 
     user_id = 0
     if trans.user:
       user_id = trans.user.id
+
+    args = [input1, input2, input3, input4, output1, output2]
+    xargs = [real_path1, input2, input3, input4, abspath_output1, abspath_output2]
     
     params = {
       "tool_id": tool_id,
-      "tool_name": "mst",
+      "tool_name": tool_name,
       "tool_version": tool_version,
       "galaxy_version": trans.app.config.version_major,
       "cwd": job_cwd,
-      "params": "%s %s %s %s %s %s" % (input1, input2, input3, input4, output1, output2),
+      "params": " ".join(args),
       "session_id": trans.galaxy_session.id,
       "user_id": user_id,
       "status": 1,
-      "output": input1_dir + "/output/output1.txt",
-      "real_output": input1dir + "/output/output1.txt",
-      "output2": input1_dir + "/output/output2.txt",
-      "real_output2": input1dir + "/output/output2.txt"
+      "output": output1,
+      "real_output": abspath_output1,
+      "output2": output2,
+      "real_output2": abspath_output2
     }
-    if input1_dir == "/":
-      params["output"] = "/output/output1.txt"
-      params["real_output"] = root_dir + "/output/output1.txt"
-      params["output2"] = "/output/output2.txt"
-      params["real_output2"] = root_dir + "/output/output2.txt"
+
     job_id = self.add_job(**params)
-    commandstr = "python job_agent.py %d mst.py %s %s %s %s %s %s %s" % (
-      job_id, job_cwd, real_path1, input2, input3, input4, output1_path, output2_path)
+    commandstr = "python job_agent.py %d %s %s %s" % (
+      job_id, script, job_cwd, " ".join(xargs))
     log.info("command = %s", commandstr)
 
     command = commandstr.split(" ")
